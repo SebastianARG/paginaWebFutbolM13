cc9679b84e3164317e05ab28fbd31e812f1516ca
alexRoche <adam23aroche@inslaferreria.cat>
2024-10-04 19:25:22 +0200

    prueba git

---
+++ prueba.java
@@ -0,0 +1,7 @@
+public class prueba{
+	private int numero:
+	
+	public prueba(int numero){this.numero = numero;}
+	public int getNumero(){return this.numero;}
+	public void setNumero(int numero){this.numero = numero;}
+}
